618774bed63445c7ec1aaa4d8bc1c8f8c89716fe
PeterKrajci <73019481+PeterKrajci@users.noreply.github.com>
2021-12-08 11:21:11 +0100

    Update cw7.js

--- first task/cw7.js
+++ first task/cw7.js
@@ -1,3 +1,15 @@
 function check(a,x){
   return a.some(item => x === item);
 };
+
+function check(a,x){
+  return a.some(item => {
+    if (typeof item === 'string' || typeof x === 'string'){
+      x = x.toLowerCase();
+      item = item.toLowerCase();
+    }
+    
+    return x === item;
+    
+  });
+};
